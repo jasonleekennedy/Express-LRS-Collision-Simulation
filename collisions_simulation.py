6860046787ceb5a9ccefd7733c82965f3a765b4f
Jason L. Kennedy <jasonleekennedy@gmail.com>
2021-10-02 16:36:02 -0500

    last phase result

--- collisions_simulation.py
+++ collisions_simulation.py
@@ -15,11 +15,12 @@ def generate_sequence(channels, packets_per_hop):
     return items
 
 
-def run_test(radios, channels, packets_per_hop):
+def run_test(radios, channels, packets_per_hop, phase):
     all_sequences = [generate_sequence(
         channels, packets_per_hop) for _ in range(radios)]
 
-    collisons = [[0 for y in range(len(all_sequences[0]))]
+    seq_len = len(all_sequences[0])
+    collisons = [[0 for y in range(seq_len)]
                  for x in range(radios)]
 
     for i in range(radios):
@@ -29,6 +30,21 @@ def run_test(radios, channels, packets_per_hop):
                     collisons[i][k] = 1
                     collisons[j][k] = 1
 
+                if phase:
+                    # phase difference collisions
+                    # we only need to check one direction, as the other direction is the same
+                    # as shifting one of the sets by 1 is the same thing
+                    k_minus = (k-1) % seq_len
+                    k_plus = (k+1) % seq_len
+                    # i hits j's prior
+                    if all_sequences[i][k_minus] == all_sequences[j][k]:
+                        collisons[i][k_minus] = 1
+                        collisons[j][k] = 1
+                    # i hits j's next
+                    if all_sequences[i][k] == all_sequences[j][k_plus]:
+                        collisons[i][k] = 1
+                        collisons[j][k_plus] = 1
+
     return collisons
 
 
@@ -36,17 +52,18 @@ def test_stats(inputs):
     radios = inputs[0]
     channels = inputs[1]
     packets_per_hop = inputs[2]
+    phase = inputs[3]
 
     total_packets = channels * packets_per_hop
 
-    return [(total_packets - sum(y))/total_packets*100 for y in run_test(radios, channels, packets_per_hop)]
+    return [(total_packets - sum(y))/total_packets*100 for y in run_test(radios, channels, packets_per_hop, phase)]
 
 
-def run_count(radios, channels, packets_per_hop):
+def run_count(radios, channels, packets_per_hop, phase):
     # Adjust based on how much forking your system can handle.
     with Pool(processes=8) as pool:
         abc = pool.map(
-            test_stats, [(radios, channels, packets_per_hop) for _ in range(1000)])
+            test_stats, [(radios, channels, packets_per_hop, phase) for _ in range(1000)])
         collision_counts = []
         for x in abc:
             collision_counts.extend(x)
@@ -60,11 +77,12 @@ def run_count(radios, channels, packets_per_hop):
 
 
 if __name__ == '__main__':
-    channels = 80
+    channels = 20
     packets_per_hop = 4
     min_radios = 2
-    max_radios = 10
+    max_radios = 50
+    phase = True
 
     print("radios,mean,stdev,min,max")
     for x in range(min_radios, max_radios):
-        run_count(x, channels, packets_per_hop)
+        run_count(x, channels, packets_per_hop, phase)
