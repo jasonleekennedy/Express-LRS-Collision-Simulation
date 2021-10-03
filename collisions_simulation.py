c3c517867ec70e41a6f7aab4482caec7044975d8
Jason L. Kennedy <jasonleekennedy@gmail.com>
2021-10-02 22:47:21 -0500

    added 1.1.0 branch shuffle simulation and adjacent channel collisions to simulation

--- collisions_simulation.py
+++ collisions_simulation.py
@@ -16,29 +16,73 @@ def sort_master(items):
         items[rand] = tmp
 
 
-def shuffle(items, python_shuffle):
-    if python_shuffle:
+def sort_110(items):
+    rng = Rng(random.randint(0, 4294967295), False)
+    is_available = [True for x in range(len(items))]
+    nr_fhss_entries = len(items)
+    nLeft = len(is_available) - 1
+    prev = 0
+
+    sequence = [0 for _ in range(256)]
+
+    for i in range(len(sequence)):
+        if i % nr_fhss_entries == 0:
+            sequence[i] = items[0]
+            prev = 0
+        else:
+            index = prev
+            while index == prev:
+                c = rng.rngN(nLeft)
+                index = 1
+                found = 0
+                while index < nr_fhss_entries:
+                    if is_available[index]:
+                        if found == c:
+                            break
+                        found += 1
+                    index += 1
+                if index == nr_fhss_entries:
+                    index = 0
+                    # print("Failed")
+                    break
+            sequence[i] = items[index]
+            is_available[index] = False
+            prev = index
+            nLeft -= 1
+            if nLeft == 0:
+                is_available = [True for x in range(len(items))]
+                nLeft = len(is_available) - 1
+
+    # print(sequence)
+    return sequence
+
+
+def shuffle(items, shuffle_type):
+    if shuffle_type == "Python":
         random.shuffle(items)
+    if shuffle_type == "1.1.0":
+        # this method always creates a 256 long list, not a multiple of the number of channels
+        items = sort_110(items)
     else:
         sort_master(items)
     return items
 
 
-def generate_sequence(channels, packets_per_hop, python_shuffle):
+def generate_sequence(channels, packets_per_hop, shuffle_type):
     items = list(range(channels))
-    items = shuffle(items, python_shuffle)
+    items = shuffle(items, shuffle_type)
 
     items = [x for x in items for i in range(packets_per_hop)]
     items = deque(items)
     # random offset of the start time of the sequence
-    items.rotate(random.randint(0, channels))
+    items.rotate(random.randint(0, len(items)))
     items = list(items)
     return items
 
 
-def run_test(radios, channels, packets_per_hop, phase, python_shuffle):
+def run_test(radios, channels, packets_per_hop, phase, shuffle_type, channel_sensitivity_width):
     all_sequences = [generate_sequence(
-        channels, packets_per_hop, python_shuffle) for _ in range(radios)]
+        channels, packets_per_hop, shuffle_type) for _ in range(radios)]
 
     seq_len = len(all_sequences[0])
     collisons = [[0 for y in range(seq_len)]
@@ -47,7 +91,7 @@ def run_test(radios, channels, packets_per_hop, phase, python_shuffle):
     for i in range(radios):
         for j in range(i + 1, radios):
             for k in range(len(all_sequences[i])):
-                if all_sequences[i][k] == all_sequences[j][k]:
+                if abs(all_sequences[i][k] - all_sequences[j][k]) <= channel_sensitivity_width:
                     collisons[i][k] = 1
                     collisons[j][k] = 1
 
@@ -58,11 +102,11 @@ def run_test(radios, channels, packets_per_hop, phase, python_shuffle):
                     k_minus = (k-1) % seq_len
                     k_plus = (k+1) % seq_len
                     # i hits j's prior
-                    if all_sequences[i][k_minus] == all_sequences[j][k]:
+                    if abs(all_sequences[i][k_minus] - all_sequences[j][k]) <= channel_sensitivity_width:
                         collisons[i][k_minus] = 1
                         collisons[j][k] = 1
-                    # i hits j's next
-                    if all_sequences[i][k] == all_sequences[j][k_plus]:
+                    # i hits j's next - i think this is equivelent to the last one when i is i + 1
+                    if abs(all_sequences[i][k] - all_sequences[j][k_plus]) <= channel_sensitivity_width:
                         collisons[i][k] = 1
                         collisons[j][k_plus] = 1
 
@@ -74,18 +118,22 @@ def test_stats(inputs):
     channels = inputs[1]
     packets_per_hop = inputs[2]
     phase = inputs[3]
-    python_shuffle = inputs[4]
+    shuffle_type = inputs[4]
+    channel_sensitivity_width = inputs[5]
 
-    total_packets = channels * packets_per_hop
+    if shuffle_type == "1.1.0":
+        total_packets = 256 * packets_per_hop
+    else:
+        total_packets = channels * packets_per_hop
 
-    return [(total_packets - sum(y))/total_packets*100 for y in run_test(radios, channels, packets_per_hop, phase, python_shuffle)]
+    return [(total_packets - sum(y))/total_packets*100 for y in run_test(radios, channels, packets_per_hop, phase, shuffle_type, channel_sensitivity_width)]
 
 
-def run_count(radios, channels, packets_per_hop, phase, python_shuffle):
+def run_count(radios, channels, packets_per_hop, phase, shuffle_type, channel_sensitivity_width):
     # Adjust based on how much forking your system can handle.
     with Pool(processes=8) as pool:
         abc = pool.map(
-            test_stats, [(radios, channels, packets_per_hop, phase, python_shuffle) for _ in range(1000)])
+            test_stats, [(radios, channels, packets_per_hop, phase, shuffle_type, channel_sensitivity_width) for _ in range(1000)])
         collision_counts = []
         for x in abc:
             collision_counts.extend(x)
@@ -104,8 +152,10 @@ if __name__ == '__main__':
     min_radios = 2
     max_radios = 50
     phase = True
-    python_shuffle = True
+    shuffle_type = "Python"  # Python, 1.1.0, Master
+    channel_sensitivity_width = 1  # 0 is only own channel, 1 is adjacent, etc.
 
     print("radios,mean,stdev,min,max")
     for x in range(min_radios, max_radios):
-        run_count(x, channels, packets_per_hop, phase, python_shuffle)
+        run_count(x, channels, packets_per_hop, phase,
+                  shuffle_type, channel_sensitivity_width)
